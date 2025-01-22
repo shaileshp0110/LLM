10e0134d146551f3c10b0739fdc6ad592df59f17
mineme0110 <shailesh.patil@iohk.io>
2025-01-22 11:54:25 +0000

    minist dagit classification
    Signed-off-by: mineme0110 <shailesh.patil@iohk.io>

--- mnist/src/mnist/__about__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2025-present mineme0110 <mineme0110@gmail.com>
-#
-# SPDX-License-Identifier: MIT
-__version__ = "0.0.1"
--- mnist/tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# SPDX-FileCopyrightText: 2025-present mineme0110 <mineme0110@gmail.com>
-#
-# SPDX-License-Identifier: MIT
